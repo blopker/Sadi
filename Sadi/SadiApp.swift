e8a8d750283a6970e8727796cd1afa41f4850772
Bo Lopker <lopkerk@gmail.com>
2026-05-26 23:21:57 -0700

    Initial Commit

---
+++ Sadi/SadiApp.swift
@@ -0,0 +1,17 @@
+//
+//  SadiApp.swift
+//  Sadi
+//
+//  Created by Bo on 5/26/26.
+//
+
+import SwiftUI
+
+@main
+struct SadiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
